a3981239d10073dd9b001d3bc675990487a57636
R Mazzu <mazzu83@gmail.com>
2021-05-07 12:02:10 +0200

    cannot unmarshal number into Go struct field .image.display_height

--- imagehost/chevereto.go
+++ imagehost/chevereto.go
@@ -76,7 +76,7 @@ type CheveretoStruct struct {
 		SizeFormatted      string `json:"size_formatted"`
 		DisplayURL         string `json:"display_url"`
 		DisplayWidth       string `json:"display_width"`
-		DisplayHeight      string `json:"display_height"`
+		DisplayHeight      int64  `json:"display_height"`
 		ViewsLabel         string `json:"views_label"`
 		LikesLabel         string `json:"likes_label"`
 		HowLongAgo         string `json:"how_long_ago"`
